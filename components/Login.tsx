bc9afb9ade73fb21321066231a88a7cb9e522705
Damian <dzoltow@gmail.com>
2022-01-31 01:42:05 +0100

    added authentication with moralis and login page layout

---
+++ components/Login.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Image from 'next/image'
+import loginImage from '../public/login.png'
+import { useMoralis } from 'react-moralis'
+
+type LoginProps = {}
+
+export default function Login({}: LoginProps) {
+  const { authenticate, isInitializing, isAuthenticating } = useMoralis()
+
+  if (isAuthenticating) {
+    return (
+      <div className="z-999 flex justify-center align-middle">
+        <h1>LOADING...</h1>
+      </div>
+    )
+  }
+
+  return (
+    <div className="relative bg-black">
+      <h1>I am login</h1>
+
+      <div className="absolute z-50 flex h-4/6 w-full flex-col items-center justify-center space-y-4">
+        <Image
+          src={loginImage}
+          width={200}
+          height={200}
+          className="rounded-md"
+        />
+
+        <button
+          className="animate-pulse rounded-lg bg-yellow-500 p-5 font-bold"
+          onClick={authenticate}
+        >
+          Login to the METAVERSE
+        </button>
+      </div>
+
+      <div className="h-screen w-full">
+        <Image
+          src="https://links.papareact.com/55n/"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  )
+}
