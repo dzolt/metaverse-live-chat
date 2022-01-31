b00c5426a8f02acf958844fd6622fa7dcdc77383
Damian <dzoltow@gmail.com>
2022-01-31 02:02:50 +0100

    fixed Moralis Provider Props compiling error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,9 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-      appId={process.env.NEXT_PUBLIC_APP_ID}
-      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+      initializeOnMount={true}
+      appId={process.env.NEXT_PUBLIC_APP_ID || ''}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL || ''}
     >
       <Component {...pageProps} />
     </MoralisProvider>
